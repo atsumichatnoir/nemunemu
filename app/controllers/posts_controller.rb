78f2d316aa43ecd0f1a126180c4cb4bc380cca6b
atsumichatnoir <a.i.0789.a@gmail.com>
2017-08-05 00:31:18 +0900

    post
    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,7 +23,7 @@ class PostsController < ApplicationController
 
   # POST /posts
   # POST /posts.json
-  def create
+,  def create
     @post = Post.new(post_params)
 
     respond_to do |format|
@@ -69,6 +69,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :post)
     end
 end

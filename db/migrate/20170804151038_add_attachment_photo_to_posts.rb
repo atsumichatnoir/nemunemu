14e256e93e5621d217e7cfe84e8dbaa1ca8d1909
atsumichatnoir <a.i.0789.a@gmail.com>
2017-08-05 00:23:34 +0900

    [5.1]
    [5.1]

--- db/migrate/20170804151038_add_attachment_photo_to_posts.rb
+++ db/migrate/20170804151038_add_attachment_photo_to_posts.rb
@@ -1,4 +1,4 @@
-class AddAttachmentPhotoToPosts < ActiveRecord::Migration
+class AddAttachmentPhotoToPosts < ActiveRecord::Migration[5.1]
   def self.up
     change_table :posts do |t|
       t.attachment :photo
